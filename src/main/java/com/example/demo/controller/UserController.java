4a9909c89000e8198c44c2f64a60b3f948de7ecf
student <student@FJ-1025>
2026-05-20 12:10:47 +0900

    ログイン機能追加

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 
 @Controller
 public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	//ログイン画面
 	@GetMapping({ "/", "/login", "/logout" })
@@ -15,20 +27,43 @@ public class UserController {
 
 	//新規登録
 	@GetMapping("/users/new")
-	public String create() {
+	public String create(Model model) {
+
+		model.addAttribute("user", new User());
 		return "newUsers";
 
 	}
 
 	//新規作成ボタンを押したとき
 	@PostMapping("/users/add")
-	public String add() {
+	public String add(User user) {
+
+		userRepository.save(user);
 		return "redirect:/login";
+
 	}
 
 	//ログインボタンをクリック
 	@PostMapping("/login")
-	public String login() {
+	public String login(
+			@RequestParam String email,
+			@RequestParam String password,
+			Model model) {
+
+		//エラーチェック
+		List<String> errorList = new ArrayList<>();
+		if (email.length() == 0 && password.length() == 0) {
+			errorList.add("メールアドレスとパスワードを入力してください");
+		} else if (email.length() == 0) {
+			errorList.add("メールアドレスは必須です");
+		} else if (password.length() == 0) {
+			errorList.add("パスワードは必須です");
+		}
+
+		if (errorList.size() > 0) {
+			model.addAttribute("errorList", errorList);
+			return "login";
+		}
 		return "redirect:/recipes";
 	}
 
