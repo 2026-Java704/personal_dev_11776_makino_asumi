df36edacfbf8bd35b5f7c9d92382160c4d2b775b
student <student@FJ-1025>
2026-05-20 10:34:36 +0900

    トップページ画面表示

--- src/main/java/com/example/demo/controller/RecipeController.java
+++ src/main/java/com/example/demo/controller/RecipeController.java
@@ -1,15 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Recipe;
+import com.example.demo.repository.RecipeRepository;
 
 @Controller
 public class RecipeController {
 
+	@Autowired
+	private RecipeRepository recipeRepository;
+
 	//レシピ画面
 	@GetMapping("/recipes")
-	public String index() {
+	public String index(Model model) {
+		List<Recipe> recipeList = recipeRepository.findAll();
+
+		model.addAttribute("recipes", recipeList);
+
 		return "recipes";
 	}
-
 }
---
+++ src/main/java/com/example/demo/entity/Recipe.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
